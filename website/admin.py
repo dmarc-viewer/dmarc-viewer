2272760ff88045dc8ee6a95f3da7bea4be2decbd
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-02-10 18:23:45 -0500

    Pep8 admin.py and add document docstring

--- website/admin.py
+++ website/admin.py
@@ -1,102 +1,157 @@
+"""
+<Program Name>
+    parse.py
+
+<Author>
+    Lukas Puehringer <luk.puehringer@gmail.com>
+
+<Started>
+    June 10, 2015
+
+<Copyright>
+    See LICENSE for licensing information.
+
+<Purpose>
+    Registers entire dmarc_viewer model for builtin admin interface.
+    Uses external super_inlines for nested web forms.
+
+"""
 from django.contrib import admin
 from super_inlines.admin import SuperInlineModelAdmin, SuperModelAdmin
-from website.models import Report, Reporter, ReportError, Record, \
-    PolicyOverrideReason, AuthResultDKIM, AuthResultSPF, View, FilterSet, \
-    ReportType, DateRange, ViewFilterField, ReportSender, ReportReceiverDomain, \
-    SourceIP, RawDkimDomain, RawDkimResult, RawSpfDomain, RawSpfResult, \
-    AlignedDkimResult, AlignedSpfResult, Disposition, MultipleDkim
+from website.models import (Report, Reporter, ReportError, Record,
+    PolicyOverrideReason, AuthResultDKIM, AuthResultSPF, View, FilterSet,
+    ReportType, DateRange, ReportSender, ReportReceiverDomain,
+    SourceIP, RawDkimDomain, RawDkimResult, RawSpfDomain, RawSpfResult,
+    AlignedDkimResult, AlignedSpfResult, Disposition, MultipleDkim)
+
 
 class PolicyOverrideReasonInline(admin.StackedInline):
     model = PolicyOverrideReason
     extra = 0
 
+
 class AuthResultSPFInline(SuperInlineModelAdmin, admin.StackedInline):
     model = AuthResultSPF
     extra = 0
+
 
 class AuthResultDKIMInline(SuperInlineModelAdmin, admin.StackedInline):
     model = AuthResultDKIM
     extra = 0
 
+
 class RecordInline(SuperInlineModelAdmin, admin.StackedInline):
     model = Record
     extra = 0
-    inlines = (PolicyOverrideReasonInline, AuthResultSPFInline, AuthResultDKIMInline,)
+    inlines = (PolicyOverrideReasonInline, AuthResultSPFInline,
+            AuthResultDKIMInline,)
+
 
 class ReportErrorInline(SuperInlineModelAdmin, admin.StackedInline):
     model = ReportError
     extra = 0
 
+
 class ReporterAdmin(SuperModelAdmin):
     model = Reporter
     list_display = ('org_name', 'email', 'extra_contact_info')
 
+
 class ReportAdmin(SuperModelAdmin):
-    list_display = ('report_id', 'date_range_begin', 'date_range_end', 'report_type', 'date_created')
+    list_display = ('report_id', 'date_range_begin', 'date_range_end',
+            'report_type', 'date_created')
     inlines = (ReportErrorInline, RecordInline,)
+
 
 admin.site.register(Report, ReportAdmin)
 admin.site.register(Reporter, ReporterAdmin)
 admin.site.register(Record)
 
 
-
 class ReportSenderInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = ReportSender
+
+
 class ReportReceiverDomainInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = ReportReceiverDomain
+
+
 class SourceIPInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = SourceIP
+
+
 class RawDkimDomainInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = RawDkimDomain
+
+
 class RawDkimResultInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = RawDkimResult
+
+
 class RawSpfDomainInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = RawSpfDomain
+
+
 class RawSpfResultInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = RawSpfResult
+
+
 class AlignedDkimResultInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = AlignedDkimResult
+
+
 class AlignedSpfResultInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = AlignedSpfResult
+
+
 class DispositionInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = Disposition
+
+
 class MultipleDkimInline(SuperInlineModelAdmin, admin.StackedInline):
     extra = 0
     model = MultipleDkim
 
+
 class FilterSetInline(SuperInlineModelAdmin, admin.StackedInline):
     model = FilterSet
     extra = 0
-    inlines = (ReportSenderInline, ReportReceiverDomainInline,\
-        SourceIPInline, RawDkimDomainInline, RawDkimResultInline,\
-         RawSpfDomainInline, RawSpfResultInline, AlignedDkimResultInline,\
+    inlines = (ReportSenderInline, ReportReceiverDomainInline,
+        SourceIPInline, RawDkimDomainInline, RawDkimResultInline,
+         RawSpfDomainInline, RawSpfResultInline, AlignedDkimResultInline,
          AlignedSpfResultInline, DispositionInline, MultipleDkimInline)
+
 
 class ReportTypeInline(SuperInlineModelAdmin, admin.StackedInline):
     model = ReportType
     extra = 0
+
+
 class DateRangeInline(SuperInlineModelAdmin, admin.StackedInline):
     model = DateRange
     extra = 1
 
+
 class ViewAdmin(SuperModelAdmin):
     inlines = (ReportTypeInline, DateRangeInline, FilterSetInline,)
 
+
 admin.site.register(View, ViewAdmin)
+
 
 class FilterSetAdmin(SuperModelAdmin):
     model = FilterSet
+
 
 admin.site.register(FilterSet, FilterSetAdmin)
 admin.site.register(ReportType)
@@ -112,7 +167,3 @@ admin.site.register(AlignedDkimResult)
 admin.site.register(AlignedSpfResult)
 admin.site.register(Disposition)
 admin.site.register(MultipleDkim)
-
-
-
-
