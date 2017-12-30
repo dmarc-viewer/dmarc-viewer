12b0bc3501f3a9228ceb5f8000f59d300845c40b
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-12-30 18:14:11 -0500

    Revert to filebased caching
    I used to use django's Memcached cache server together with
    the recommended python-memcached binding.
    https://docs.djangoproject.com/en/1.8/topics/cache/#memcached
    
    python-memcached seems rather outdated, pylibmc might be a better
    candidate for now. For now, I just revert to filebased caching I
    had used before.

--- dmarc_viewer/settings.py
+++ dmarc_viewer/settings.py
@@ -105,14 +105,14 @@ DATABASES = {
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
     # 'default': {
-    #     'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-    #     'LOCATION': '/var/tmp/django_cache',
+    #     'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+    #     'LOCATION': '127.0.0.1:11211',
     # }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
 }
 
 
