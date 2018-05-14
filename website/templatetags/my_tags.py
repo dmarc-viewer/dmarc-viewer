4821c8e6636d3edb23e28b19d51ce37e27421e7f
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-05-14 13:50:24 +0200

    Facelift filter overview in deep analysis
    Revisit custom template tags that generate the joined filter
    field values for a filter set, to write cleaner markup and update
    related styles.

--- website/templatetags/my_tags.py
+++ website/templatetags/my_tags.py
@@ -2,17 +2,35 @@ from django import template
 
 register = template.Library()
 
-@register.simple_tag
-def join_filter_set_field_values(filter_set_fields, name, join_string = ', ', get_display=False):
-    output = ""
-    if (filter_set_fields):
-        output = name
+@register.assignment_tag
+def join_filter_set_field_values(filter_set_fields, get_display):
+    joined_filter_set_fields = ""
+    if filter_set_fields:
         if get_display:
-            output += ": " + join_string.join([f.get_value_display() for f in filter_set_fields])
+            joined_filter_set_fields += " or ".join(["'{}'".format(field.get_value_display())
+                    for field in filter_set_fields])
         else:
-            output += ": " + join_string.join([str(f.value) for f in filter_set_fields])
+            joined_filter_set_fields += " or ".join(["'{}'".format(field.value)
+                    for field in filter_set_fields])
 
-    return output
+    return joined_filter_set_fields
+
+
+@register.assignment_tag
+def get_filter_set_field_tuples(filter_set):
+    return [
+        ("Report Sender Email", filter_set.reportsender_set.all, False),
+        ("Report Receiver Domain", filter_set.reportreceiverdomain_set.all, False),
+        ("Mail Sender Source IP", filter_set.sourceip_set.all, False),
+        ("Aligned DKIM Result", filter_set.aligneddkimresult_set.all, True),
+        ("Aligend SPF Result", filter_set.alignedspfresult_set.all, True),
+        ("Disposition", filter_set.disposition_set.all, True),
+        ("Raw SPF Domain", filter_set.rawspfdomain_set.all, False),
+        ("Raw SPF Result", filter_set.rawspfresult_set.all, True),
+        ("Raw DKIM Domain", filter_set.rawdkimdomain_set.all, False),
+        ("Raw DKIM Result", filter_set.rawdkimresult_set.all, True),
+        ("Multiple DKIM Only", filter_set.multipledkim_set.all, False),
+    ]
 
 
 @register.inclusion_tag('website/view-editor-filterset.html')
