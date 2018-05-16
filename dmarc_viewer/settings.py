8550b42428ec69887a75dd9b88940f9ccae67bc1
Lukas Puehringer <lukas.puehringer@nyu.edu>
2018-05-16 11:51:37 +0200

    Add comment to settings.py

--- dmarc_viewer/settings.py
+++ dmarc_viewer/settings.py
@@ -59,6 +59,8 @@ BOOTSTRAP3 = {
     'set_required' : False
 }
 
+# This dict is available in any templates via a custom context processor.
+# See `website.context` for more info
 TEMPLATE_SETTINGS = {
     "use_dist": True
 }
@@ -146,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
